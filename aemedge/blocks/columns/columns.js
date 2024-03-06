490297c1f07455367ba761eb015f34123092943b
Luca Nerlich <luca.nerlich@googlemail.com>
2024-03-06 09:53:48 +0100

    10 - start with hover column variation

--- aemedge/blocks/columns/columns.js
+++ aemedge/blocks/columns/columns.js
@@ -2,15 +2,27 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
-  // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
+      // setup image columns
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
         if (picWrapper && picWrapper.children.length === 1) {
           // picture is only content in column
           picWrapper.classList.add('columns-img-col');
+        }
+
+        // setup hover variation
+        if (block.classList.contains('hover')) {
+          // remove p-parent of picture tag
+          // const pictureParagraph = pic.parentElement;
+          // pictureParagraph.parentNode.replaceChild(pic, pictureParagraph);
+
+          // set image as background of col div
+
+          col.style.backgroundImage = `url(${pic.querySelector('img')?.src})`;
+          pic.parentElement.remove();
         }
       }
     });

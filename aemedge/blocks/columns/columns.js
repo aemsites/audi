f83fb34cdf9784d14b4b4c2731249015e19f6d61
Luca Nerlich <luca.nerlich@googlemail.com>
2024-03-06 09:54:15 +0100

    10 - start with hover column variation

--- aemedge/blocks/columns/columns.js
+++ aemedge/blocks/columns/columns.js
@@ -15,12 +15,7 @@ export default function decorate(block) {
 
         // setup hover variation
         if (block.classList.contains('hover')) {
-          // remove p-parent of picture tag
-          // const pictureParagraph = pic.parentElement;
-          // pictureParagraph.parentNode.replaceChild(pic, pictureParagraph);
-
-          // set image as background of col div
-
+          // set image as background of col div and remove picture element
           col.style.backgroundImage = `url(${pic.querySelector('img')?.src})`;
           pic.parentElement.remove();
         }

b8c4ebc32f09313241de3d7f2627e04b94fc6aa4
Sean Steimer <ssteimer@adobe.com>
2024-03-05 18:14:15 -0800

    full dom implementation of header

--- aemedge/blocks/header/header.js
+++ aemedge/blocks/header/header.js
@@ -3,6 +3,66 @@ import { getMetadata, decorateIcons, toClassName } from '../../scripts/aem.js';
 // media query match that indicates mobile/tablet width
 const isDesktop = window.matchMedia('(min-width: 900px)');
 
+/**
+ * Toggles all nav sections
+ * @param {Element} sections The container element
+ * @param {Boolean} expanded Whether the sections should be expanded or collapsed
+ */
+function toggleAllNavSections(sections, expanded = false) {
+  sections.querySelectorAll('.nav-section-flyout').forEach((sectionFlyout) => {
+    sectionFlyout.setAttribute('aria-expanded', expanded);
+  });
+}
+
+function closeOnEscape(nav, navSections) {
+  const navSectionExpanded = navSections.querySelector('[aria-expanded="true"]');
+  const menuExpanded = nav.getAttribute('aria-expanded') === 'true';
+  if (navSectionExpanded) {
+    // eslint-disable-next-line no-use-before-define
+    toggleAllNavSections(navSections);
+    navSectionExpanded.focus();
+  } else if (!isDesktop.matches && menuExpanded) {
+    // eslint-disable-next-line no-use-before-define
+    toggleMenu(nav, navSections);
+    nav.querySelector('button').focus();
+  }
+}
+
+function headerKeyPress(event, nav, navSections) {
+  if (event.code === 'Escape') {
+    closeOnEscape(nav, navSections);
+  } else {
+    // todo revisit keybaord support for section toggle and section close
+    const focused = document.activeElement;
+    if (focused.classList.contains('nav-section-toggle-button')
+    && event.code === 'Space') {
+      event.preventDefault();
+      const flyout = document.getElementById(focused.getAttribute('aria-controls'));
+      if (flyout) {
+        const expanded = flyout.getAttribute('aria-expanded') === 'true';
+        toggleAllNavSections(navSections);
+        flyout.setAttribute('aria-expanded', !expanded);
+      }
+    }
+  }
+}
+
+/**
+ * toggle the menu open state
+ * @param {Element} nav the nav
+ * @param {Element} navList the nav list (ul)
+ * @param {boolean} forceExpanded if menu should be forced open instead of toggled
+ */
+function toggleMenu(nav, navSections, forceExpanded = null) {
+  const expanded = forceExpanded !== null ? !forceExpanded : nav.getAttribute('aria-expanded') === 'true';
+  const button = nav.querySelector('.nav-hamburger button');
+  document.body.style.overflowY = (expanded || isDesktop.matches) ? '' : 'hidden';
+  nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+  button.setAttribute('aria-label', `${expanded ? 'Open' : 'Close'} ${button.querySelector('.nav-hamburger-label').textContent}`);
+  // close all nav section
+  toggleAllNavSections(navSections, false);
+}
+
 function linkFromObject(obj) {
   const a = document.createElement('a');
   a.href = obj.Url;
@@ -19,15 +79,23 @@ function createNavSection(navSectionJson, sectionCount) {
   li.classList.add('nav-section');
   const sectionLink = linkFromObject(navSectionJson.Link);
   sectionLink.setAttribute('role', 'button');
+  sectionLink.classList.add('nav-section-toggle-button');
   li.append(sectionLink);
   const sectionId = toClassName(`nav-section-${sectionLink.textContent}-${sectionCount}`);
   sectionLink.setAttribute('aria-controls', sectionId);
-  sectionLink.setAttribute('aria-expanded', false);
 
   const flyout = document.createElement('div');
   flyout.classList.add('nav-section-flyout');
+  flyout.setAttribute('aria-expanded', false);
   flyout.id = sectionId;
   li.append(flyout);
+
+  const flyoutCloseButton = document.createElement('button');
+  flyoutCloseButton.setAttribute('type', 'button');
+  flyoutCloseButton.setAttribute('aria-controls', sectionId);
+  flyoutCloseButton.className = 'nav-flyout-close-button';
+  flyoutCloseButton.innerHTML = sectionLink.innerHTML;
+  flyout.append(flyoutCloseButton);
 
   const ul = document.createElement('ul');
   ul.append(...navSectionJson.SubNavigation.map((subNav) => {
@@ -48,6 +116,7 @@ function createNavSection(navSectionJson, sectionCount) {
   teaserImg.alt = navSectionJson.Teaser.Image.Alt;
   teaserImg.width = navSectionJson.Teaser.Image.Width;
   teaserImg.height = navSectionJson.Teaser.Image.Height;
+  teaserImg.setAttribute('loading', 'lazy');
   teaser.append(teaserImg);
   teaser.append(linkFromObject(navSectionJson.Teaser.Link));
   flyout.append(teaser);
@@ -74,11 +143,21 @@ function buildNav(navJson) {
   // sections
   const sections = document.createElement('div');
   sections.className = 'nav-sections';
-  const ul = document.createElement('ul');
-  ul.classList.add('nav-list');
-  ul.id = 'nav-list';
-  ul.append(...navJson.MainNavigation.map(createNavSection));
-  sections.append(ul);
+  const navList = document.createElement('ul');
+  navList.classList.add('nav-list');
+  navList.append(...navJson.MainNavigation.map(createNavSection));
+  navList.querySelectorAll('.nav-section-toggle-button, .nav-flyout-close-button').forEach((sectionButton) => {
+    sectionButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      const flyout = document.getElementById(sectionButton.getAttribute('aria-controls'));
+      if (flyout) {
+        const expanded = flyout.getAttribute('aria-expanded') === 'true';
+        toggleAllNavSections(sections, false);
+        flyout.setAttribute('aria-expanded', !expanded);
+      }
+    });
+  });
+  sections.append(navList);
 
   // tools
   const tools = document.createElement('div');
@@ -96,12 +175,12 @@ function buildNav(navJson) {
   nav.append(brand, sections, tools);
 
   // hamburger
+  const hamburger = document.createElement('div');
+  hamburger.classList.add('nav-hamburger');
   const hamburgerButton = document.createElement('button');
-  hamburgerButton.classList.add('nav-hamburger');
   hamburgerButton.setAttribute('type', 'button');
-  hamburgerButton.setAttribute('aria-controls', 'nav-list');
+  hamburgerButton.setAttribute('aria-controls', 'nav');
   hamburgerButton.setAttribute('aria-label', `Open ${navJson.MenuLabel}`);
-  hamburgerButton.setAttribute('aria-expanded', 'false');
   const hamburgerButtonIcon = document.createElement('span');
   hamburgerButtonIcon.className = 'nav-hamburger-icon';
   hamburgerButton.append(hamburgerButtonIcon);
@@ -109,14 +188,22 @@ function buildNav(navJson) {
   hamburgerButtonText.textContent = navJson.MenuLabel;
   hamburgerButtonText.classList.add('nav-hamburger-label');
   hamburgerButton.append(hamburgerButtonText);
+  hamburger.append(hamburgerButton);
 
-  nav.prepend(hamburgerButton);
+  nav.prepend(hamburger);
+  nav.setAttribute('aria-expanded', 'false');
+  nav.id = 'nav';
 
-  // hamburgerButton.addEventListener('click', () => toggleMenu(nav, sections));
+  hamburgerButton.addEventListener('click', () => toggleMenu(nav, sections));
 
-  // // prevent mobile nav behavior on window resize
-  // toggleMenu(nav, sections, isDesktop.matches);
-  // isDesktop.addEventListener('change', () => toggleMenu(nav, sections, isDesktop.matches));
+  // prevent mobile nav behavior on window resize
+  toggleMenu(nav, sections, isDesktop.matches);
+  isDesktop.addEventListener('change', () => toggleMenu(nav, sections, isDesktop.matches));
+
+  // keyboard nav accessibility
+  window.addEventListener('keydown', (e) => {
+    headerKeyPress(e, nav, sections);
+  });
 
   decorateIcons(nav);
   return nav;
@@ -124,10 +211,26 @@ function buildNav(navJson) {
 
 export default async function decorate(block) {
   const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta).pathname : '/uk/web/en/tools/nemo/navigation/oneheader/_jcr_content.json';
-  const resp = await fetch(`https://www.audi.co.uk${navPath}`); // todo cors
-  if (resp.ok) {
-    const navJson = await resp.json();
+  let navJson;
+  try {
+    const navPath = navMeta ? new URL(navMeta).pathname : '/uk/web/en/tools/nemo/navigation/oneheader/_jcr_content.json';
+    const resp = await fetch(`https://www.audi.co.uk${navPath}`);
+    if (resp.ok) {
+      navJson = await resp.json();
+    }
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error('Failed to fetch audi nav json (likely due to CORS). Falling back to local.', e);
+  }
+
+  if (!navJson) {
+    const resp = await fetch(`${window.hlx.codeBasePath}/blocks/header/nav-fallback.json`);
+    if (resp.ok) {
+      navJson = await resp.json();
+    }
+  }
+
+  if (navJson) {
     const nav = buildNav(navJson);
 
     const navWrapper = document.createElement('div');
